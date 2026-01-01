cac69d6259d055073464c081fd3aef4b16ea976d
Luan Campos <campos.luan@outlook.com>
2026-01-01 10:40:25 -0300

    Change DB

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://okpdeaxwabkopkiqzhpc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rcGRlYXh3YWJrb3BraXF6aHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwOTYwNDIsImV4cCI6MjA4MjY3MjA0Mn0.5fb1wlbkvjtlAkcaH7TJdiOfPctv-wn-HbLbeNTdMa8';
+const supabaseUrl = 'https://iambwblptokdiwjkztss.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhbWJ3YmxwdG9rZGl3amt6dHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5OTg3MTYsImV4cCI6MjA4MTU3NDcxNn0.w_il9JwSo8NFscHQ6aaJ9LNlpy06fbBh54sgg9FU1tw';
 
 /**
  * Single Supabase client for the entire application
